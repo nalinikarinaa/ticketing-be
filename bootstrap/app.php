70542c81b20d005a8e77cc1f1f017e088df82b73
Nalini Karina Andhini <nalinikarina613@gmail.com>
2026-01-20 21:09:10 +0700

    nambah routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,16 +7,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',   // ⬅️ WAJIB biar API kebaca
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        // Sanctum (cookie auth FE)
+        $middleware->append(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
 
-        $middleware->alias([
-            'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
+        // CORS
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 Route::get('/test', function () {
     return response()->json([
         'message' => 'API OK',
     ]);
 });
+
+Route::post('/register', [AuthController::class, 'register']);
